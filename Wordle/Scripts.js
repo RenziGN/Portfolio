9eaef790d08d2b9d3e1b1592d9723d3906944b38
RenziGN <renzogola2003@gmail.com>
2025-11-06 21:28:00 -0300

    Se arreglo el flujo y se subdividio el codigo para poder mejorar el mantenimietno

--- Wordle/Scripts.js
+++ Wordle/Scripts.js
@@ -1,24 +1,34 @@
 /* 
  Pasar toda variable de texto a uppercase
 */
+//  TODO  
+//! URGENTE
+//? 
 // <>
 const palabras = ["Bytes","Disco","Botón","Cable","Macro","Clase","Clave","Tabla","Datos","Codec"];
 const allLetters = /[a-zA-Z]/g;
-let palabraJuego = "";
+
 let input1 = " ";
 let intentos = 0;
 let condicionvictoria = false;
 let cantAciertos=  0;
 let contadorUbicacion = 1;
 let teclaPresionada = "";
+let limiteInf = 1;
+let limiteSup = 5;
+
 
 saberTeclaInput(contadorUbicacion);
+IniciarJuego();
 
 function IniciarJuego(){ //Script de inicio de juego
     let palabraJugador = ""
+    let palabraJuego = "";
+    console.log(contadorUbicacion + " - " + limiteInf )
+    
     palabraJuego = ElegirPalabraAleatoria() // Esta funcion selecciona y asigna una palabra aleatoria del array
     palabraJuego.toUpperCase(); // CONVERTIMOS EL STRING A MAYUSCULAS
-    
+    /*
     while(intentos < 6 && condicionvictoria !== true ){
         palabraJugador = "";
         for(i = 0 ; i < 5 ; i++){
@@ -31,6 +41,7 @@ function IniciarJuego(){ //Script de inicio de juego
         CompararPalabra(palabraJugador,palabraJuego); 
         intentos++;
     }
+    */
 }
 
 /*
@@ -47,9 +58,9 @@ function Checkletra(palabra,letraingre,){ //Esta funcion chequea
         }
     
 }
-*/
+*/  
 
-function CompararPalabra(palabraJ,palabraM){ //
+function CompararPalabra(palabraJ,palabraM){ //* Esta funcion esta correcta
     cantAciertos = 0
 
     for(i = 0 ; i < 5 ; i++){ // *RECORRE Y COMPARA LA PALABRA DEL JUEGO CONTRA LA DEL JUGADOR
@@ -62,7 +73,6 @@ function CompararPalabra(palabraJ,palabraM){ //
             console.log(`La letra ${letraJ} es correcta en esta posición`)
             cantAciertos = cantAciertos + 1;
             console.log(cantAciertos);
-           
             
         }else if(palabraJuego.includes(letraJ)){
             //? LA PALABRA DEL JUGADOR ESTÁ CONTENIDA DENTRO DE LA PALABRA DEL JUEGO
@@ -79,27 +89,21 @@ function CompararPalabra(palabraJ,palabraM){ //
     }
 }
 
-function saberTeclaInput(){
+function saberTeclaInput(){ //* Esta funcion esta correcta
     window.addEventListener('keydown', function (e) {
     teclaPresionada = e.key.toUpperCase();
-        if (teclaPresionada.match(allLetters) && teclaPresionada.length==1 || teclaPresionada === "BACKSPACE" ) // toma el backspace ademas del rango determinado
+        if (teclaPresionada.match(allLetters) && teclaPresionada.length==1) // toma el backspace ademas del rango determinado
         {
-            if (teclaPresionada === "BACKSPACE") //si backspace es verdadero
-            {
-            verificadorInput(teclaPresionada); // verifica si es correcta
-            document.querySelector(`#p${contadorUbicacion - 1 }`).innerHTML = " "; //si es backspace borra el ultimo input
-            contadorUbicacion--; //baja el contador 1 para poder sobreescribir la letra
-            }
-            else
-            {
-            document.querySelector(`#p${contadorUbicacion}`).innerHTML = `${teclaPresionada}`; //escribe el input segun donde este el contador de ubicacion
-            verificadorInput(teclaPresionada); // verifica la tecla
-            contadorUbicacion++; //Aumenta en 1 el contador de ubicacion
-            if (verificarfinaljuego(contadorUbicacion) === false)
+            
+            Agregarletra();
+            
+            if (verificarfinaljuego())
             {
                 alert("Fin del juego");
             }
-            }
+        }
+        else if(teclaPresionada === "BACKSPACE"){
+            BorrarLetra()
         }
         else
         {
@@ -111,21 +115,9 @@ function saberTeclaInput(){
 }
 
 
-function verificarfinaljuego(estado) //esto verifica el estado del juego final
+function verificarfinaljuego() //esto verifica el estado del juego final
 { 
-    if (estado === 26)
-    {
-        return false
-    }
-    else
-    {
-        return true
-    }
-}
-
-
-function verificadorInput(a){ //Verifica si el input es backspace
-    if (a === "BACKSPACE")
+    if (contadorUbicacion > limiteSup)
     {
         return true
     }
@@ -136,9 +128,41 @@ function verificadorInput(a){ //Verifica si el input es backspace
 }
 
 
-function ElegirPalabraAleatoria(){
+function ElegirPalabraAleatoria(){//* Esta funcion esta correcta
     let randomPos = Math.floor(Math.random() * palabras.length) // esta funcion elige aleatoriamente una de las palabras del array Palabras
     return palabras[randomPos]
 }
 
-IniciarJuego()
+
+
+function Agregarletra(){ //* Esta funcion esta correcta
+    if (contadorUbicacion > limiteSup)
+    {
+        alert("NO SE PUEDE");
+    }
+    else
+    {
+        document.querySelector(`#p${contadorUbicacion}`).innerHTML = `${teclaPresionada}`; //escribe el input segun donde este el contador de ubicacion
+        //TODO hacer la verificacion del limite 
+        contadorUbicacion++; //Aumenta en 1 el contador de ubicacion
+
+    }
+}
+
+function BorrarLetra() //* Esta funcion esta correcta
+{
+    console.log(contadorUbicacion + " - " + limiteInf )
+    
+    if (contadorUbicacion > limiteInf ) 
+    {
+        contadorUbicacion--; //baja el contador 1 para poder sobreescribir la letra
+        document.querySelector(`#p${contadorUbicacion }`   ).innerHTML = " "; //si es backspace borra el ultimo input
+        console.log("LUEGO DE BORRAR - CONT UBI" + contadorUbicacion)
+    } 
+    else
+    {
+        console.log(contadorUbicacion);
+        console.log(limiteInf);
+        alert("No se pueden retroceder mas casillas");
+    }
+}
